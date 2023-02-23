bf9cc7ab9960310b2e5ba42d803b690e3dca20de
codermauro <mauro.druwel@gmail.com>
2023-02-23 10:35:11 +0100

    Sync

--- LoRa_Test_Receiver/LoRa_Test_Receiver.ino
+++ LoRa_Test_Receiver/LoRa_Test_Receiver.ino
@@ -9,15 +9,16 @@ to show updates on the connection status and data.
 @author Enrico Miglino <enrico.miglino@gmail.com>
 @version 1.0.1
 */
+#include <SPI.h>
 #include <LoRa.h>
 #include <Streaming.h>
 #include <Streaming.h>
-#include "oled.h"
+//#include "oled.h"
 
 #define SYS_LED 6
 
 //! Dispaly instance
-Adafruit_SSD1306 oled = Adafruit_SSD1306(OLED_WIDTH, OLED_HEIGHT, &Wire, -1);
+//Adafruit_SSD1306 oled = Adafruit_SSD1306(OLED_WIDTH, OLED_HEIGHT, &Wire, -1);
 
 /**
 Initialization function
@@ -34,12 +35,12 @@ void setup() {
 
   Serial << "LoRa Receiver" << endl;
 
-  if (!LoRa.begin(915E6)) {
+  if (!LoRa.begin(868E6)) {
     Serial <<"Starting LoRa failed! System halted." << endl;
     while (1);
   }
 
-  // Initialize the display
+  /*// Initialize the display
   oled.begin(SSD1306_SWITCHCAPVCC, OLED_I2C);
   oled.setRotation(2); // Rotate the display 180 Deg
 
@@ -65,15 +66,15 @@ void setup() {
 
   oled.clearDisplay();
   oled.display();
-
+  */
 }
 
 void loop() {
   int packetSize = LoRa.parsePacket();
 
-  if(Serial1.available()) {
+  /*if(Serial1.available()) {
     Serial << "From PSoC6 " << Serial1.readString() << endl;
-  }
+  }*/
   
   if (packetSize) {
     digitalWrite(SYS_LED, HIGH);
@@ -87,155 +88,3 @@ void loop() {
     digitalWrite(SYS_LED, LOW);
   }
 }
-
-// =========================================================
-//                      Oled functions
-// =========================================================
-
-/**
- * Show the text string at the desired coordinates.
- * 
- * The text is shown according to the current settings (color, font, etc.)
- * 
- * \param text The string of text to display
- * \param x The x cursor coordinates
- * \param y The y cursor coordinates
- * \param color The color of the text
- * \param disp Pointer to the Oled display class
- */
-void showText(char* text, int x, int y, int color, Adafruit_SSD1306* disp) {
-  // Set the desired color
-  switch(color) {
-    case COL_WHITE:
-    oled.setTextColor(SSD1306_WHITE);
-    break;
-  }
-  disp->setCursor(x, y);
-  disp->print(text);
-  disp->display();
-}
-
-/**
- * Initialize the display before showing a new screen.
- * 
- * \note This method should be called for first before setting a new
- * string or when the screen setting changes.
- * 
- * \param disp Pointer to the Oled display class
- */
-void initDisplay(Adafruit_SSD1306* disp) {
-  disp->clearDisplay();
-  disp->display();
-}
-
-/**
- * Set the desired font and size to the text.
- * 
- * \param fontName One of the following font families:\n
- * \param fontSize The size in point of the font chosen between one of the
- * following values: 9, 12, 18, 24
- * \param disp Pointer to the Oled display class
- * 
- * \return 0 if the font is set else return -1
- */
-int textFont(int fontName, int fontSize, Adafruit_SSD1306* disp) {
-  int j;
-
-  // Check for the font size
-  if( (fontSize != 9) && (fontSize != 12) &&
-      (fontSize != 18) &&(fontSize != 24) ) {
-        return -1;
-      }
-
-  // Switch the font family
-  switch(fontName) {
-    case MONO:
-    #ifndef FREE_MONO
-    disp->setFont();
-    return -1;
-    #else
-    switch(fontSize) {
-      case 9:
-        disp->setFont(&FreeMono9pt7b);
-      break;
-      case 12:
-        disp->setFont(&FreeMono12pt7b);
-      break;
-      case 18:
-        disp->setFont(&FreeMono18pt7b);
-      break;
-      case 24:
-        disp->setFont(&FreeMono24pt7b);
-      break;
-    }
-    #endif
-    break;
-
-    case MONO_BOLD:
-    #ifndef FREE_MONO_BOLD
-    disp->setFont();
-    return -1;
-    #else
-    switch(fontSize) {
-      case 9:
-        disp->setFont(&FreeMonoBold9pt7b);
-      break;
-      case 12:
-        disp->setFont(&FreeMonoBold12pt7b);
-      break;
-      case 18:
-        disp->setFont(&FreeMonoBold18pt7b);
-      break;
-      case 24:
-        disp->setFont(&FreeMonoBold24pt7b);
-      break;
-    }
-    #endif
-    break;
-
-    case MONO_OBLIQUE:
-    #ifndef FREE_MONO_OBLIQUE
-    disp->setFont();
-    return -1;
-    #else
-    switch(fontSize) {
-      case 9:
-        disp->setFont(&FreeMonoOblique9pt7b);
-      break;
-      case 12:
-        disp->setFont(&FreeMonoOblique12pt7b);
-      break;
-      case 18:
-        disp->setFont(&FreeMonoOblique18pt7b);
-      break;
-      case 24:
-        disp->setFont(&FreeMonoOblique24pt7b);
-      break;
-    }
-    #endif
-    break;
-
-    case MONO_BOLD_OBLIQUE:
-    #ifndef FREE_MONO_BOLD_OBLIQUE
-    disp->setFont();
-    return -1;
-    #else
-    switch(fontSize) {
-      case 9:
-        disp->setFont(&FreeMonoBoldOblique9pt7b);
-      break;
-      case 12:
-        disp->setFont(&FreeMonoBoldOblique12pt7b);
-      break;
-      case 18:
-        disp->setFont(&FreeMonoBoldOblique18pt7b);
-      break;
-      case 24:
-        disp->setFont(&FreeMonoBoldOblique24pt7b);
-      break;
-    }
-    #endif
-    break;
-  } 
-}
-
--- LoRa_Test_Sender/LoRa_Test_Sender.ino
+++ LoRa_Test_Sender/LoRa_Test_Sender.ino
@@ -2,8 +2,8 @@
 @file LoRa_Test_Sender
 @brief Arduino MKR 1300 WAN LoRa WAN sending test
 
-It is expected that the sender module (onboard of the drone) 
-will try to connect to the ground receiver when the receiver is already 
+It is expected that the sender module (onboard of the drone)
+will try to connect to the ground receiver when the receiver is already
 
 @date April, 2021
 @author Enrico Miglino <enrico.miglino@gmail.com>
@@ -15,24 +15,24 @@ will try to connect to the ground receiver when the receiver is already
 #include <Streaming.h>
 
 int counter = 0;
-String val="";
+String val = "";
 
 /**
-Initialize the Lora connection with the gound device. 
+Initialize the Lora connection with the gound device.
 */
 void setup() {
- if (!LoRa.begin(915E6)) {
-    Serial.println("Starting LoRa failed!");
-    while (1);
-  }
+	if (!LoRa.begin(868E6)) {
+		Serial.println("Starting LoRa failed!");
+		while (1);
+	}
 }
 
 void loop() {
-  LoRa.beginPacket();
-  LoRa << "Nanodrone test " << counter << endl;
-  LoRa.endPacket();
-  counter++; // this help keep track if the packet is recived on the sender side
+	LoRa.beginPacket();
+	LoRa << "CoderMauro's test " << counter << endl;
+	LoRa.endPacket();
+	counter++; // this help keep track if the packet is recived on the sender side
 
-  // Wait about a minute
-  delay(58000);
+	// Wait about a second
+	delay(1000);
 }
